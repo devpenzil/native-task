e393d48d76443172d20bedb39799b68e6c73331e
devpenzil <ajoalex012@gmail.com>
2022-06-09 19:35:40 +0530

    snapshot

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,56 @@
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, Share} from 'react-native';
+// import {Share} from 'react-native-share';
 import React from 'react';
-
+import {styles} from './Home.style';
+import ViewShot from 'react-native-view-shot';
+import data from './data.json';
 const Home = () => {
+  const viewShotRef = React.useRef(null);
+  //   const options = {
+  //     title: 'Share via',
+  //     messgae: 'shgsjh',
+  //   };
+  const shareSnap = () => {
+    viewShotRef.current.capture().then(uri => {
+      console.log(uri);
+      //   Share.open(options)
+      //     .then(res => {
+      //       console.log(res);
+      //     })
+      //     .catch(err => {
+      //       err && console.log(err);
+      //     });
+      Share.share({
+        message: uri,
+        url: uri,
+        title: 'Share via',
+      });
+    });
+    error => {
+      console.log(error);
+    };
+  };
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <ViewShot ref={viewShotRef}>
+        <View style={styles.card}>
+          <FlatList
+            data={data}
+            renderItem={({item}) => {
+              return (
+                <View style={styles.element}>
+                  <Text style={styles.elementText}>
+                    {item.id} - {item.name}
+                  </Text>
+                </View>
+              );
+            }}
+          />
+        </View>
+      </ViewShot>
+      <TouchableOpacity style={styles.shareButton} onPress={shareSnap}>
+        <Text style={styles.shareText}>Share</Text>
+      </TouchableOpacity>
     </View>
   );
 };
--- src/Home.style.js
+++ src/Home.style.js
@@ -1,2 +1,33 @@
 import {StyleSheet} from 'react-native';
-export const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#cecece',
+  },
+  card: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 8,
+    margin: 20,
+  },
+  element: {
+    padding: 5,
+  },
+  shareButton: {
+    backgroundColor: '#fcfcfc',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    padding: 14,
+  },
+  shareText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '700',
+    color: '#000',
+  },
+  elementText: {
+    color: '#000',
+  },
+});
